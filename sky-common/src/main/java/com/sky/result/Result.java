308368202b55fee993c9003e12c02b1b079d3303
yxy2176 <344561707@qq.com>
2026-03-13 21:54:08 +0800

    feat: 新增员工部分

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -6,30 +6,34 @@ import java.io.Serializable;
 
 /**
  * 后端统一返回结果
+ * 统一结果封装类
+ * 核心作用：标准化后端接口返回给前端的数据格式
  * @param <T>
  */
 @Data
+// implements Serializable： 实现序列化接口
+// => 意味着该类的对象可以被转换为字节流，以便在网络中传输或者存入缓存（如Redis）中
 public class Result<T> implements Serializable {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
-    private String msg; //错误信息
-    private T data; //数据
+    private Integer code; // 业务状态码：通常约定 1 表示成功，0和其它数字为失败  => 让前端能快速判断该请求是否处理成功，而不仅仅是依赖HTTP状态码
+    private String msg; // 错误信息（提示信息） => 当后端报错，如“密码错误”的时候，会把具体的原因存放在这里，展示给前端看。
+    private T data; // 泛型数据载荷 => 这是真正要传给前端的业务数据（如用户信息，菜品列表等），使用泛型 <T> 是为了让它可以装下任何类型的数据
 
     public static <T> Result<T> success() {
-        Result<T> result = new Result<T>();
+        Result<T> result = new Result<>();
         result.code = 1;
         return result;
     }
 
     public static <T> Result<T> success(T object) {
-        Result<T> result = new Result<T>();
+        Result<T> result = new Result<>();
         result.data = object;
         result.code = 1;
         return result;
     }
 
     public static <T> Result<T> error(String msg) {
-        Result result = new Result();
+        Result<T> result = new Result<>();
         result.msg = msg;
         result.code = 0;
         return result;
--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -8,6 +9,7 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +23,7 @@ import java.util.Map;
 /**
  * 员工管理
  */
+// RestController 是 @Controller 和 @ResponseBody 的组合注解
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
@@ -63,6 +66,20 @@ public class EmployeeController {
                 .build();
 
         return Result.success(employeeLoginVO);
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工：{}", employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success();
+
     }
 
     /**
